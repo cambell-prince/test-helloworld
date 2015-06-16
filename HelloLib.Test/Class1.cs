ec2077971f603d36c12e81fc1a670bfc1d17246c
cambell <cambell.prince@gmail.com>
2015-06-16 11:46:04 +0700

    Initial import

---
+++ HelloLib.Test/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace HelloLib.Test
+{
+    [TestFixture]
+    public class TestClass1
+    {
+        [Test]
+        public void TestSayHello_ReturnsHello()
+        {
+            string result = Class1.SayHello();
+            Assert.AreEqual("Hello World", result);
+        }
+
+        [Test]
+        public void TestSagHallo_ReturnsHallo()
+        {
+            string result = Class1.SagHallo();
+            Assert.AreEqual("Hallo Welt!", result);
+        }
+
+    }
+}
---
+++ HelloLib/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloLib
+{
+    public class Class1
+    {
+        public static string SayHello()
+        {
+            return "Hello World";
+        }
+
+        // German
+        public static string SagHallo()
+        {
+            return "Hallo Welt!";
+        }
+    }
+}
